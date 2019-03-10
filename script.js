06a00ee9c906e6d7b7040edbecebb3f5b30bb67e
haroon <haroonrasheed333@gmail.com>
2019-03-09 18:50:56 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+window.onload = function() {
+};
+
+
+
+//get the lat and long for any location entered in the textbox - done
+//enter the lat and long as input to display the weathern-doing
+//display a map for the location entered in the textbox - done
+//enter the weather at the current location as default -done
+//display the icon depending on the weather condition - done
